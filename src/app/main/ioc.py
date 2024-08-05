1c75e045c1968dcdb3eb45d53cbc881252e7a0d2
Alexander <qrer71@yandex.ru>
2024-08-05 14:18:59 +0300

    Add ioc

---
+++ src/app/main/ioc.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterator
+from contextlib import contextmanager
+
+from app.adapter.repository.user import UserRepository
+from app.application.usecase.user import UserUseCase
+from app.presentation.interactor import InteractorFactory
+
+
+class IoC(InteractorFactory):
+    def __init__(
+            self,
+            user_repository: UserRepository
+    ) -> None:
+        self.user_repository = user_repository
+
+    @contextmanager
+    def add_user_usecase(self) -> Iterator[UserUseCase]:
+        yield UserUseCase(
+            repository=self.user_repository  # type: ignore
+        )
