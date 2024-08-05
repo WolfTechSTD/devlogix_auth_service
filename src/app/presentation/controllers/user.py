45eb1abbf5c09f441be8748f47ddaf67efbe2610
Alexander <qrer71@yandex.ru>
2024-08-05 15:43:01 +0300

    Update method to create a user

--- src/app/presentation/controllers/user.py
+++ src/app/presentation/controllers/user.py
@@ -1,10 +1,12 @@
 from typing import Annotated
 
-from litestar import Controller, get, post
+from litestar import Controller, post, status_codes
+from litestar.exceptions import HTTPException
 from litestar.params import Dependency
 
-from app.application.model.user import CreateUserDTO, UserDTO
+from app.application.exceptions import UserExistsException
 from app.presentation.interactor import InteractorFactory
+from app.presentation.model.user import JsonCreateUser, JsonUser
 
 
 class UserController(Controller):
@@ -13,8 +15,15 @@ class UserController(Controller):
     @post()
     async def create_user(
             self,
-            data: CreateUserDTO,
+            data: JsonCreateUser,
             ioc: Annotated[InteractorFactory, Dependency(skip_validation=True)]
-    ) -> UserDTO:
-        with ioc.add_user_usecase() as user_use_case:
-            return await user_use_case.create_user(data)
+    ) -> JsonUser:
+        try:
+            with ioc.add_user_usecase() as user_use_case:
+                user = await user_use_case.create_user(data.into(data))
+                return JsonUser.into(user)
+        except UserExistsException as err:
+            raise HTTPException(
+                status_code=status_codes.HTTP_400_BAD_REQUEST,
+                detail=str(err)
+            )
