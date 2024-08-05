0541277310df9249f307dbf46bccc004ffa740f8
Alexander <qrer71@yandex.ru>
2024-08-05 14:20:36 +0300

    Add di

--- src/app/main/app.py
+++ src/app/main/app.py
@@ -1,6 +1,25 @@
 from litestar import Litestar
+from litestar.di import Provide
+
+from app.adapter.persistence.connect import (
+    create_async_session_maker,
+)
+from app.adapter.repository.user import UserRepository
+from app.presentation.controllers.user import UserController
+from .ioc import IoC
 
 
 def create_app() -> Litestar:
-    app = Litestar(route_handlers=[])
+    session_factory = create_async_session_maker()
+
+    app = Litestar(
+        route_handlers=[UserController],
+        dependencies={
+            "session": Provide(
+                session_factory
+            ),
+            "user_repository": Provide(UserRepository),
+            "ioc": Provide(IoC)
+        }
+    )
     return app
