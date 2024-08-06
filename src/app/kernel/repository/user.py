fbc91a54730d36ac42cc2736d0e6cf7f4d435468
Alexander <qrer71@yandex.ru>
2024-08-06 22:58:14 +0300

    Add methods to check a user

--- src/app/kernel/repository/user.py
+++ src/app/kernel/repository/user.py
@@ -1,12 +1,30 @@
 from abc import abstractmethod
 
-from app.kernel.model.user import User, NewUser
+from app.kernel.model.id import Id
+from app.kernel.model.user import User, NewUser, UpdateUser
 from .base import Repository
 
 
-class UserRepository(Repository[NewUser, User]):
+class UserRepository(Repository[NewUser, UpdateUser, User]):
     @abstractmethod
     async def get_total(self) -> int: ...
 
     @abstractmethod
     async def check_user(self, username: str, email: str) -> bool: ...
+
+    @abstractmethod
+    async def check_user_exists(self, user_id: Id) -> bool: ...
+
+    @abstractmethod
+    async def check_username_exists(
+            self,
+            user_id: Id,
+            username: str
+    ) -> bool: ...
+
+    @abstractmethod
+    async def check_email_exists(
+            self,
+            user_id: Id,
+            email: str
+    ) -> bool: ...
