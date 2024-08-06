9542a00a51b4447f16d16555b8fe01acd487ed5b
Alexander <qrer71@yandex.ru>
2024-08-06 22:57:16 +0300

    Add new exceptions

--- src/app/application/exceptions.py
+++ src/app/application/exceptions.py
@@ -10,3 +10,13 @@ class UserExistsException(UserException):
 class UserNotFoundException(UserExistsException):
     def __str__(self) -> str:
         return "Пользователь не найден"
+
+
+class UserWithUsernameExistsException(UserExistsException):
+    def __str__(self) -> str:
+        return "Пользователь с таким юзернейм уже существует"
+
+
+class UserWithEmailExistsException(UserExistsException):
+    def __str__(self) -> str:
+        return "Пользователь с такой почтой уже существует"
