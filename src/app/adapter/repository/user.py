16c877b6781e085e4be700f04dfe29e070c16f85
Alexander <qrer71@yandex.ru>
2024-08-06 13:14:26 +0300

    Add method save

--- src/app/adapter/repository/user.py
+++ src/app/adapter/repository/user.py
@@ -2,6 +2,7 @@ from collections.abc import Iterator
 from typing import Any
 
 from sqlalchemy import insert, select, or_, func
+from ulid import ULID
 
 from app.adapter.model import Users
 from .base import DatabaseRepository
@@ -9,9 +10,8 @@ from .base import DatabaseRepository
 
 class UserRepository(DatabaseRepository[Users]):
     async def create_user(self, **kwargs: Any) -> Users:
-        stmt = insert(Users).values(**kwargs).returning(Users)
+        stmt = insert(Users).values(id=str(ULID()), **kwargs).returning(Users)
         result = (await self.session.execute(stmt)).scalar_one()
-        await self.session.commit()
         return result
 
     async def get_user(self, **kwargs: Any) -> Users | None:
@@ -23,13 +23,14 @@ class UserRepository(DatabaseRepository[Users]):
             self,
             limit: int,
             offset: int
-    ) -> tuple[Iterator[Users], int]:
-        stmt = select(func.count(Users.id))
-        total = await self.session.scalar(stmt)
-
+    ) -> Iterator[Users]:
         stmt = select(Users).limit(limit).offset(offset)
         result = await self.session.execute(stmt)
-        return result.scalars(), total
+        return result.scalars()
+
+    async def get_total(self) -> int:
+        stmt = select(func.count(Users.id))
+        return await self.session.scalar(stmt)
 
     async def check_user(self, username: str, email: str) -> bool:
         stmt = select(Users).where(
@@ -39,3 +40,6 @@ class UserRepository(DatabaseRepository[Users]):
             )
         ).exists()
         return await self.session.scalar(select(stmt))
+
+    async def save(self) -> None:
+        await self.session.commit()
