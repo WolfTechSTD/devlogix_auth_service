6d510a7f7dd3262cb071d6277b18356d43e9d528
Alexander <qrer71@yandex.ru>
2024-08-06 20:58:08 +0300

    Add base repository

---
+++ src/app/kernel/repository/base.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod
+from collections.abc import Iterator
+from typing import Protocol, TypeVar
+
+from app.kernel.model.id import Id
+
+NewModel = TypeVar("NewModel")
+Model = TypeVar("Model")
+
+
+class Repository(Protocol[NewModel, Model]):
+    @abstractmethod
+    async def insert(self, source: NewModel) -> Model: ...
+
+    @abstractmethod
+    async def get(self, source: Id) -> Model | None: ...
+
+    @abstractmethod
+    async def get_list(self, limit: int, offset: int) -> Iterator[Model]: ...
+
+    @abstractmethod
+    async def save(self) -> None: ...
--- src/app/kernel/repository/user.py
+++ src/app/kernel/repository/user.py
@@ -1,30 +1,12 @@
 from abc import abstractmethod
-from collections.abc import Iterator
-from typing import Protocol
 
-from app.kernel.model.id import Id
 from app.kernel.model.user import User, NewUser
+from .base import Repository
 
 
-class UserRepository(Protocol):
-    @abstractmethod
-    async def insert(self, source: NewUser) -> User: ...
-
-    @abstractmethod
-    async def get(self, source: Id) -> User | None: ...
-
-    @abstractmethod
-    async def get_list(
-            self,
-            limit: int,
-            offset: int
-    ) -> Iterator[User]: ...
-
+class UserRepository(Repository[NewUser, User]):
     @abstractmethod
     async def get_total(self) -> int: ...
 
     @abstractmethod
     async def check_user(self, username: str, email: str) -> bool: ...
-
-    @abstractmethod
-    async def save(self) -> None: ...
