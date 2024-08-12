768ba7bc364af419a1638881548f90ec150f6b50
Alexander <qrer71@yandex.ru>
2024-08-12 19:41:22 +0300

    Add method to check username and email

--- src/app/application/usecase/user.py
+++ src/app/application/usecase/user.py
@@ -18,6 +18,7 @@ from app.application.model.user import (
     UpdateUserView, UserLoginView,
 )
 from app.kernel.model.id import Id
+from app.kernel.permissions.user import UserPermissions
 from app.kernel.repository.cookie_token import CookieTokenRepository
 from app.kernel.repository.user import UserRepository
 from app.kernel.security.password import PasswordProvider
@@ -28,11 +29,13 @@ class UserUseCase:
             self,
             repository: UserRepository,
             password_provider: PasswordProvider,
-            cookie_token_repository: CookieTokenRepository
+            cookie_token_repository: CookieTokenRepository,
+            user_permissions: UserPermissions | None
     ) -> None:
         self.repository = repository
         self.password_provider = password_provider
         self.cookie_token_repository = cookie_token_repository
+        self.user_permissions = user_permissions
 
     async def create_user(self, source: CreateUserView) -> UserView:
         if await self.repository.check_user(
@@ -40,6 +43,7 @@ class UserUseCase:
                 email=source.email
         ):
             raise UserExistsException()
+
         source.password = self.password_provider.get_password_hash(
             source.password
         )
@@ -49,41 +53,38 @@ class UserUseCase:
 
     async def login(self, source: UserLoginView) -> CookieTokenView:
         user = await self.repository.get_user(source.username, source.email)
+
         if user is None:
             raise UserLoginException()
+
         if not self.password_provider.verify_password(
                 source.password,
                 user.password
         ):
             raise UserLoginException()
+
         if source.token is not None:
             await self.cookie_token_repository.destroy(
                 source.token
             )
+
         cookie_token = await self.cookie_token_repository.write(
             source.create_token(user).into()
         )
         return CookieTokenView.from_into(cookie_token)
 
     async def update_user(self, source: UpdateUserView) -> UserView:
+        await self.user_permissions.check_cookie_token(source.token)
+
         if not await self.repository.check_user_exists(cast(Id, source.id)):
             raise UserNotFoundException()
-        err = {
-            "username": await self.repository.check_username_exists(
-                user_id=cast(Id, source.id),
-                username=source.username or ""
-            ),
-            "email": await self.repository.check_email_exists(
-                cast(Id, source.id),
-                source.email or ""
+        await self._check_username_and_email(source)
+
+        if source.password is not None:
+            source.password = self.password_provider.get_password_hash(
+                source.password
             )
-        }
-        if err["email"] and err["username"]:
-            raise UserWithEmailAndUsernameExistsException()
-        elif err["email"]:
-            raise UserWithEmailExistsException()
-        elif err["username"]:
-            raise UserWithUsernameExistsException()
+
         user = await self.repository.update(source.into())
         await self.repository.save()
         return UserView(
@@ -93,10 +94,13 @@ class UserUseCase:
             is_active=user.is_active
         )
 
-    async def get_user(self, user_id: str) -> UserView:
+    async def get_user(self, user_id: str, token: str) -> UserView:
+        await self.user_permissions.check_cookie_token(token)
+
         user = await self.repository.get(cast(Id, user_id))
         if user is None:
             raise UserNotFoundException()
+
         return UserView(
             id=str(user.id),
             username=user.username,
@@ -107,8 +111,11 @@ class UserUseCase:
     async def get_users(
             self,
             limit: int,
-            offset: int
+            offset: int,
+            token: str
     ) -> UserListView:
+        await self.user_permissions.check_cookie_token(token)
+
         users = await self.repository.get_list(
             limit=limit,
             offset=limit * offset
@@ -123,3 +130,22 @@ class UserUseCase:
                 is_active=user.is_active
             ) for user in users)
         )
+
+    async def _check_username_and_email(self, source: UpdateUserView) -> None:
+        err = {
+            "username": await self.repository.check_username_exists(
+                user_id=cast(Id, source.id),
+                username=source.username or ""
+            ),
+            "email": await self.repository.check_email_exists(
+                cast(Id, source.id),
+                source.email or ""
+            )
+        }
+
+        if err["email"] and err["username"]:
+            raise UserWithEmailAndUsernameExistsException()
+        elif err["email"]:
+            raise UserWithEmailExistsException()
+        elif err["username"]:
+            raise UserWithUsernameExistsException()
