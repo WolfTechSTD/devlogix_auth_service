3ed5fa914397c0f475e12c8209a5d13bf700654a
Alexander <qrer71@yandex.ru>
2024-08-22 14:24:58 +0300

    Update imports

--- src/app/presentation/interactor.py
+++ src/app/presentation/interactor.py
@@ -1,14 +1,14 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod, ABC
 from contextlib import AbstractAsyncContextManager
 
+from app.application.interfaces import UserPermissionCookie
 from app.application.usecase.user import UserUseCase
-from app.kernel.permissions.user import UserPermissions
 
 
 class InteractorFactory(ABC):
     @abstractmethod
     def user_usecase(
             self,
-            user_permissions: UserPermissions | None = None
+            user_permissions: UserPermissionCookie | None = None
     ) -> AbstractAsyncContextManager[UserUseCase]:
         ...
