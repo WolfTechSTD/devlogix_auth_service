cb2b51c236c42c08c379acf28a17cb2528e610d0
Alexander <qrer71@yandex.ru>
2024-08-24 20:38:13 +0300

    Update config

--- src/app/config.py
+++ src/app/config.py
@@ -13,6 +13,8 @@ class DatabaseConfig:
 class JWTConfig:
     secret_key: str
     algorithm: str
+    assess_token_time: int
+    refresh_token_time: int
 
 
 @dataclass
@@ -36,17 +38,19 @@ def get_str_env(key: str) -> str:
 
 
 def _load_redis_config() -> RedisConfig:
-    return RedisConfig(url=os.getenv('REDIS_URL'))
+    return RedisConfig(url=get_str_env('REDIS_URL'))
 
 
 def _load_database_config() -> DatabaseConfig:
-    return DatabaseConfig(db_url=os.getenv("DATABASE_URL"))
+    return DatabaseConfig(db_url=get_str_env("DATABASE_URL"))
 
 
 def _load_jwt_config() -> JWTConfig:
     return JWTConfig(
-        secret_key=os.getenv("SECRET_KEY"),
-        algorithm=os.getenv("ALGORITHM")
+        secret_key=get_str_env("SECRET_KEY"),
+        algorithm=get_str_env("ALGORITHM"),
+        assess_token_time=int(get_str_env("ASSESS_TOKEN_TIME")),
+        refresh_token_time=int(get_str_env("REFRESH_TOKEN_TIME"))
     )
 
 
