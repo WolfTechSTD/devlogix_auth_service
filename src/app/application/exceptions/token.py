51a56dee507a4b09e1e4d33d550c78fae696b989
Alexander <qrer71@yandex.ru>
2024-08-25 12:58:07 +0300

    Add new exception

--- src/app/application/exceptions/token.py
+++ src/app/application/exceptions/token.py
@@ -5,3 +5,8 @@ class TokenException(Exception):
 class InvalidTokenException(TokenException):
     def __str__(self) -> str:
         return "Доступ запрещен"
+
+
+class TokenTimeException(TokenException):
+    def __str__(self) -> str:
+        return "Пользователь на авторизован"
