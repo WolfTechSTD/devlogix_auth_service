c8dbf844c7cd79b54b8071016244a0232a38f1df
Alexander <qrer71@yandex.ru>
2024-08-25 12:57:42 +0300

    Update imports and method decode

--- src/app/application/interfaces/jwt.py
+++ src/app/application/interfaces/jwt.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 from app.domain.model.id import Id
+from app.domain.model.token import AccessToken
 
 
 class AJWTProvider(ABC):
@@ -9,7 +10,7 @@ class AJWTProvider(ABC):
     def encode(self, data: dict[str, Any]) -> str: ...
 
     @abstractmethod
-    def decode(self, token: str) -> dict[str, Any]: ...
+    def decode(self, token: AccessToken) -> dict[str, Any]: ...
 
     @abstractmethod
     def get_access_token(self, user_id: Id) -> str: ...
