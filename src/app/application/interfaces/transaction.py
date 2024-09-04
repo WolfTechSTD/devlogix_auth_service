2e522d5d798a084c5e99ddc4ec5ca6e87f1ffe03
Alexander <qrer71@yandex.ru>
2024-09-04 16:14:35 +0300

    Update Transaction

--- src/app/application/interfaces/transaction.py
+++ src/app/application/interfaces/transaction.py
@@ -2,9 +2,15 @@ from abc import abstractmethod
 from typing import Protocol
 
 
-class Transaction(Protocol):
+class Transaction(
+    Protocol
+):
     @abstractmethod
-    async def refresh(self): ...
+    async def rollback(
+            self
+    ) -> None: ...
 
     @abstractmethod
-    async def commit(self): ...
+    async def commit(
+            self
+    ) -> None: ...
