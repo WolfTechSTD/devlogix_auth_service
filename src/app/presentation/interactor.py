d7cbdb6604e9c97a1517ebcc19433cc06d3f9a01
Alexander <qrer71@yandex.ru>
2024-09-13 15:47:36 +0300

    Add InteractorFactory

--- src/app/presentation/interactor.py
+++ src/app/presentation/interactor.py
@@ -1,17 +1,55 @@
 from abc import abstractmethod, ABC
 from contextlib import AbstractAsyncContextManager
 
-from app.application.interfaces import UserPermission
-from app.application.usecase.jwt import JWTUseCase
-from app.application.usecase.user import UserUseCase
+from app.application.interfaces import IUserPermission
+from app.application.usecase.jwt.delete_refresh_token import DeleteRefreshToken
+from app.application.usecase.jwt.get_tokens import GetTokens
+from app.application.usecase.jwt.update_accesss_token import UpdateAccessToken
+from app.application.usecase.jwt.update_refresh_token import UpdateRefreshToken
+from app.application.usecase.user.create_user import CreateUser
+from app.application.usecase.user.delete_user_me import DeleteUserMe
+from app.application.usecase.user.get_user_me import GetUserMe
+from app.application.usecase.user.update_user_me import UpdateUserMe
 
 
 class InteractorFactory(ABC):
     @abstractmethod
-    def user_usecase(
+    def create_user(
             self,
-            user_permission: UserPermission
-    ) -> AbstractAsyncContextManager[UserUseCase]: ...
+            user_permission: IUserPermission
+    ) -> AbstractAsyncContextManager[CreateUser]: ...
 
     @abstractmethod
-    def jwt_usecase(self) -> AbstractAsyncContextManager[JWTUseCase]: ...
+    def get_tokens(
+            self,
+            user_permission: IUserPermission
+    ) -> AbstractAsyncContextManager[GetTokens]: ...
+
+    @abstractmethod
+    def update_access_token(
+            self,
+            user_permission: IUserPermission
+    ) -> AbstractAsyncContextManager[UpdateAccessToken]: ...
+
+    @abstractmethod
+    def update_refresh_token(
+            self,
+            user_permission: IUserPermission
+    ) -> AbstractAsyncContextManager[UpdateRefreshToken]: ...
+
+    @abstractmethod
+    def delete_refresh_token(
+            self
+    ) -> AbstractAsyncContextManager[DeleteRefreshToken]: ...
+
+    @abstractmethod
+    def get_user_me(self) -> AbstractAsyncContextManager[GetUserMe]: ...
+
+    @abstractmethod
+    def update_user_me(
+            self,
+            user_permission: IUserPermission
+    ) -> AbstractAsyncContextManager[UpdateUserMe]: ...
+
+    @abstractmethod
+    def delete_user_me(self) -> AbstractAsyncContextManager[DeleteUserMe]: ...
