a1b0fbef17e84207776b5d062c6c4a21e3214750
Alexander <qrer71@yandex.ru>
2024-09-13 15:49:28 +0300

    Update model

--- src/app/domain/model/token.py
+++ src/app/domain/model/token.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
+import datetime as dt
 
 
 @dataclass(slots=True, kw_only=True)
 class RedisToken:
     key: str
     value: str | None = field(default=None)
-    lifetime_second: int | None = field(default=None)
+    lifetime_second: int | dt.timedelta | None = field(default=None)
 
 
 @dataclass(slots=True)
