953248c2ebfc5e35bf8c0da0765213991fa0fab7
Alexander <qrer71@yandex.ru>
2024-09-13 15:47:47 +0300

    Update IoC

--- src/app/ioc.py
+++ src/app/ioc.py
@@ -9,13 +9,15 @@ from app.adapter.db.gateway import (
     UserGateway,
     RefreshTokenGateway,
 )
-from app.adapter.permission import UserPermission
-from app.adapter.security import (
-    PasswordProvider,
-    JWTProvider,
-)
-from app.application.usecase.jwt import JWTUseCase
-from app.application.usecase.user import UserUseCase
+from app.application.interfaces import IUserPermission
+from app.application.usecase.jwt.delete_refresh_token import DeleteRefreshToken
+from app.application.usecase.jwt.get_tokens import GetTokens
+from app.application.usecase.jwt.update_accesss_token import UpdateAccessToken
+from app.application.usecase.jwt.update_refresh_token import UpdateRefreshToken
+from app.application.usecase.user.create_user import CreateUser
+from app.application.usecase.user.delete_user_me import DeleteUserMe
+from app.application.usecase.user.get_user_me import GetUserMe
+from app.application.usecase.user.update_user_me import UpdateUserMe
 from app.presentation.interactor import InteractorFactory
 
 
@@ -23,37 +25,89 @@ class IoC(InteractorFactory):
     def __init__(
             self,
             session: AsyncSession,
-            password_provider: PasswordProvider,
             strategy: RedisStrategy,
-            jwt_provider: JWTProvider,
             refresh_token_time: int
     ) -> None:
         self.transaction = get_transaction(session)
         self.user_gateway = UserGateway(session)
-        self.password_provider = password_provider
         self.strategy = strategy
-        self.refresh_token_gateway = RefreshTokenGateway(session, refresh_token_time)
-        self.jwt_provider = jwt_provider
+        self.refresh_token_gateway = RefreshTokenGateway(
+            session,
+            refresh_token_time
+        )
 
     @asynccontextmanager
-    async def user_usecase(
+    async def create_user(
             self,
-            user_permission: UserPermission
-    ) -> AsyncIterator[UserUseCase]:
-        yield UserUseCase(
+            user_permission: IUserPermission
+    ) -> AsyncIterator[CreateUser]:
+        yield CreateUser(
             transaction=self.transaction,
             user_gateway=self.user_gateway,
-            password_provider=self.password_provider,
-            strategy_redis=self.strategy,
             user_permission=user_permission
         )
 
     @asynccontextmanager
-    async def jwt_usecase(self) -> AsyncIterator[JWTUseCase]:
-        yield JWTUseCase(
+    async def get_tokens(
+            self,
+            user_permission: IUserPermission
+    ) -> AsyncIterator[GetTokens]:
+        yield GetTokens(
+            transaction=self.transaction,
+            user_permission=user_permission,
+            user_gateway=self.user_gateway,
+            refresh_token_gateway=self.refresh_token_gateway,
+        )
+
+    @asynccontextmanager
+    async def update_access_token(
+            self,
+            user_permission: IUserPermission
+    ) -> AsyncIterator[UpdateAccessToken]:
+        yield UpdateAccessToken(
+            transaction=self.transaction,
+            user_permission=user_permission,
+            refresh_token_gateway=self.refresh_token_gateway,
+        )
+
+    @asynccontextmanager
+    async def update_refresh_token(
+            self,
+            user_permission: IUserPermission
+    ) -> AsyncIterator[UpdateRefreshToken]:
+        yield UpdateRefreshToken(
+            transaction=self.transaction,
+            user_permission=user_permission,
+            refresh_token_gateway=self.refresh_token_gateway,
+        )
+
+    @asynccontextmanager
+    async def delete_refresh_token(self) -> AsyncIterator[DeleteRefreshToken]:
+        yield DeleteRefreshToken(
             transaction=self.transaction,
             refresh_token_gateway=self.refresh_token_gateway,
+        )
+
+    @asynccontextmanager
+    async def get_user_me(self) -> AsyncIterator[GetUserMe]:
+        yield GetUserMe(
             user_gateway=self.user_gateway,
-            jwt_provider=self.jwt_provider,
-            password_provider=self.password_provider
+        )
+
+    @asynccontextmanager
+    async def update_user_me(
+            self,
+            user_permission: IUserPermission
+    ) -> AsyncIterator[UpdateUserMe]:
+        yield UpdateUserMe(
+            transaction=self.transaction,
+            user_permission=user_permission,
+            user_gateway=self.user_gateway,
+        )
+
+    @asynccontextmanager
+    async def delete_user_me(self) -> AsyncIterator[DeleteUserMe]:
+        yield DeleteUserMe(
+            transaction=self.transaction,
+            user_gateway=self.user_gateway,
         )
