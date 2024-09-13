613a8f5ab52c328dcb205e3c78de93a71ac00239
Alexander <qrer71@yandex.ru>
2024-09-13 15:43:21 +0300

    Add class BaseMiddleware

---
+++ src/app/presentation/middleware/base.py
@@ -0,0 +1,10 @@
+from litestar.middleware.base import MiddlewareProtocol
+from litestar.types import (
+    ASGIApp,
+)
+
+
+class BaseMiddleware(MiddlewareProtocol):
+    def __init__(self, app: ASGIApp) -> None:
+        super().__init__(app)
+        self.app = app
