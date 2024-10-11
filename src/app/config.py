a2a83dd7af6cc71eba823cc6a8ad1a1f20b1c902
Alexander <qrer71@yandex.ru>
2024-10-11 13:33:04 +0300

    Update imports

--- src/app/config.py
+++ src/app/config.py
@@ -1,7 +1,7 @@
 import os
 from dataclasses import dataclass
 
-from app.exceptions.base import ConfigParseError
+from app.exceptions import ConfigParseError
 
 
 @dataclass
@@ -37,23 +37,8 @@ def get_str_env(key: str) -> str:
     return value
 
 
-def _load_cors_config() -> CORSConfig:
-    return CORSConfig(
-        allow_origins=get_str_env("ALLOW_ORIGINS").split(","),
-    )
-
-
 def load_database_config() -> DatabaseConfig:
     return DatabaseConfig(db_url=get_str_env("DATABASE_URL"))
-
-
-def _load_jwt_config() -> JWTConfig:
-    return JWTConfig(
-        secret_key=get_str_env("SECRET_KEY"),
-        algorithm=get_str_env("ALGORITHM"),
-        assess_token_time=int(get_str_env("ASSESS_TOKEN_TIME")),
-        refresh_token_time=int(get_str_env("REFRESH_TOKEN_TIME"))
-    )
 
 
 def load_config() -> ApplicationConfig:
@@ -62,4 +47,19 @@ def load_config() -> ApplicationConfig:
         db=load_database_config(),
         jwt=_load_jwt_config(),
         cors=_load_cors_config(),
+    )
+
+
+def _load_cors_config() -> CORSConfig:
+    return CORSConfig(
+        allow_origins=get_str_env("ALLOW_ORIGINS").split(","),
+    )
+
+
+def _load_jwt_config() -> JWTConfig:
+    return JWTConfig(
+        secret_key=get_str_env("SECRET_KEY"),
+        algorithm=get_str_env("ALGORITHM"),
+        assess_token_time=int(get_str_env("ASSESS_TOKEN_TIME")),
+        refresh_token_time=int(get_str_env("REFRESH_TOKEN_TIME"))
     )
