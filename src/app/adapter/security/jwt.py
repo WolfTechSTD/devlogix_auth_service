027750ff3064e402b88fc7758eb23a4453a000e6
Alexander <qrer71@yandex.ru>
2024-10-15 18:52:56 +0300

    Update TokenProvider

--- src/app/adapter/security/jwt.py
+++ src/app/adapter/security/jwt.py
@@ -5,27 +5,24 @@ from typing import Any
 import jwt
 from jwt import ExpiredSignatureError, DecodeError
 
-from app.exceptions import InvalidTokenException
-from app.exceptions.token import TokenTimeException
 from app.application.interface import ITokenProvider
+from app.config import JWTConfig
 from app.domain.model.id import Id
 from app.domain.model.token import AccessToken
+from app.exceptions import InvalidTokenException
+from app.exceptions.token import TokenTimeException
 
 
 class TokenProvider(ITokenProvider):
     def __init__(
             self,
-            key: str,
-            algorithm: str,
-            time_access_token: int
+            config: JWTConfig
     ) -> None:
-        self.key = key
-        self.algorithm = algorithm
-        self.time_access_token = time_access_token
+        self.config = config
 
     def get_access_token(self, user_id: Id) -> str:
         date_on = dt.datetime.now(dt.timezone.utc)
-        exp = date_on + dt.timedelta(minutes=self.time_access_token)
+        exp = date_on + dt.timedelta(minutes=self.config.assess_token_time)
         return self.encode(
             {
                 "id": user_id,
@@ -40,16 +37,16 @@ class TokenProvider(ITokenProvider):
     def encode(self, data: dict[str, Any]) -> str:
         return jwt.encode(
             payload=data,
-            key=self.key,
-            algorithm=self.algorithm
+            key=self.config.secret_key,
+            algorithm=self.config.algorithm
         )
 
     def decode(self, token: AccessToken) -> dict[str, Any]:
         try:
             token = jwt.decode(
                 token.value,
-                self.key,
-                algorithms=[self.algorithm]
+                self.config.secret_key,
+                algorithms=[self.config.algorithm]
             )
             return token
         except ExpiredSignatureError:
