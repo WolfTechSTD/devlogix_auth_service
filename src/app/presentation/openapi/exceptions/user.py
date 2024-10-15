c87c6aed06a1998cac3f1b47ba74ffc812753318
Alexander <qrer71@yandex.ru>
2024-10-15 12:19:53 +0300

    Delete module

--- src/app/presentation/openapi/exceptions/user.py
+++
@@ -1,9 +0,0 @@
-USER_EXISTS_EXCEPTION = {
-    "status_code": 400,
-    "detail": "Пользователь уже существует"
-}
-
-USER_NOT_FOUND_EXCEPTION = {
-    "status_code": 404,
-    "detail": "Пользователь не найден"
-}
