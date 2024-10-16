65422ae34cd8e4754f2e284f315db31b1062f81e
Alexander <qrer71@yandex.ru>
2024-10-16 14:02:33 +0300

    Add function to update the user

--- src/app/presentation/broker/user.py
+++ src/app/presentation/broker/user.py
@@ -2,15 +2,24 @@ from dishka.integrations.base import FromDishka as Depends
 from faststream.kafka import KafkaRouter
 
 from app.presentation.interactor import InteractorFactory
-from app.presentation.model.user import JsonCreateUser
+from app.presentation.model.user import JsonCreateUser, JsonUpdateUser
 
 UserController = KafkaRouter()
 
 
-@UserController.subscriber("auth")
+@UserController.subscriber("create_user")
 async def create_user(
         msg: JsonCreateUser,
         ioc: Depends[InteractorFactory],
 ) -> None:
     async with ioc.user_usecase() as usecase:
         await usecase.create_user(msg.into())
+
+
+@UserController.subscriber("update_user")
+async def update_user(
+        msg: JsonUpdateUser,
+        ioc: Depends[InteractorFactory],
+) -> None:
+    async with ioc.user_usecase() as usecase:
+        await usecase.update_user(msg.into())
