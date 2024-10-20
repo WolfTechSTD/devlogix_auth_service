6069c85c4815cb08a90faf9bf7c5730f5d1c45ee
Alexander <qrer71@yandex.ru>
2024-10-20 19:05:10 +0300

    Update engine

--- src/app/adapter/persistence/db.py
+++ src/app/adapter/persistence/db.py
@@ -10,5 +10,14 @@ from app.config import DatabaseConfig
 def new_session_maker(
         config: DatabaseConfig
 ) -> async_sessionmaker[AsyncSession]:
-    engine = create_async_engine(config.db_url)
-    return async_sessionmaker(engine, expire_on_commit=False)
+    engine = create_async_engine(
+        config.db_url,
+        pool_size=15,
+        max_overflow=15,
+    )
+    return async_sessionmaker(
+        engine,
+        class_=AsyncSession,
+        expire_on_commit=False,
+        autoflush=False,
+    )
